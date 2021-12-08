df965f4ada125b3205626c170586aa152cabe772
Geoffrey Clark <g.clark@samsung.com>
2021-12-08 17:00:29 -0500

    vimdiff as git diff tool

--- emacs/myinit.el
+++ emacs/myinit.el
@@ -337,3 +337,10 @@ supported in Scala."
 
 (require 'loadhist)
 (file-dependents (feature-file 'cl))
+
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+(add-hook 'yaml-mode-hook
+  '(lambda ()
+     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
